cab003c7750bb5069cc284d5c7e8b0aff5b0a79b
Ahmed TAHRI <tahri.ahmed@proton.me>
2026-08-12 07:12:39 +0200

    perf: shortcut for iana_name function with newest const prebuilt dict

--- src/charset_normalizer/utils.py
+++ src/charset_normalizer/utils.py
@@ -5,7 +5,6 @@ import logging
 import unicodedata
 from bisect import bisect_right
 from codecs import IncrementalDecoder
-from encodings.aliases import aliases
 from functools import lru_cache
 from typing import Generator
 
@@ -302,12 +301,9 @@ def iana_name(cp_name: str, strict: bool = True) -> str:
     """Returns the Python normalized encoding name (Not the IANA official name)."""
     cp_name = cp_name.lower().replace("-", "_")
 
-    encoding_alias: str
-    encoding_iana: str
-
-    for encoding_alias, encoding_iana in aliases.items():
-        if cp_name in [encoding_alias, encoding_iana]:
-            return encoding_iana
+    encoding_iana = _IANA_NAMES.get(cp_name)
+    if encoding_iana is not None:
+        return encoding_iana
 
     if strict:
         raise ValueError(f"Unable to retrieve IANA for '{cp_name}'")
